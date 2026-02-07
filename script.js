a04d0f548ab060817c93c50ba050418d9cb60a09
Maggie-sis <lenafrancesca7@gmail.com>
2026-02-07 21:03:29 +0100

    Add active class to current navbar link

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ const navbarMenu = document.querySelector(".navbar-menu");
 navbarToggle.addEventListener("click", () => {
   navbarToggle.classList.toggle("active");
   navbarMenu.classList.toggle("active");
-})
+});
+const links = document.querySelectorAll(".navbar-menu a");
+const currentPage = location.pathname.split("/").pop();
+
+links.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
